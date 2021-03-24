b52c646fe892a050eb80f31344d0a9d0f2703383
sraboni303 <sraboni391@gmail.com>
2021-03-24 06:18:18 +0600

    Login system via email & phone_number & username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,35 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        $login_info = request()->input('login_info');
+        $phone_number = substr($login_info, 0, 4);
+
+        if( filter_var($login_info, FILTER_VALIDATE_EMAIL) ){
+
+            $name = 'email';
+
+        }elseif($phone_number == 8801){
+
+            $name = 'phone_number';
+
+        }else{
+            $name = 'username';
+        }
+
+
+        request()->merge([$name => $login_info]);
+        return $name;
+
+    }
+
+
 }

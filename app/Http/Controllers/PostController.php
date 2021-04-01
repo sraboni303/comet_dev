cfec83865c30bb9fba3b810e823cfa4c940a2939
sraboni303 <sraboni391@gmail.com>
2021-04-02 02:32:31 +0600

    validate posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -30,9 +30,8 @@ class PostController extends Controller
     public function store(Request $request){
 
         $this->validate($request, [
-            'title' => 'required',
+            'title' => 'required|unique:posts|max:255',
             'content' => 'required',
-
         ]);
     }
 }

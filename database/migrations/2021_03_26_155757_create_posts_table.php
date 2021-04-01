792a4fc5262ce0bdf1320cb3c6a8f7cf905c17a1
sraboni303 <sraboni391@gmail.com>
2021-04-02 01:58:35 +0600

    design posts schema

--- database/migrations/2021_03_26_155757_create_posts_table.php
+++ database/migrations/2021_03_26_155757_create_posts_table.php
@@ -15,6 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('content');
+            $table->longText('featured')->nullable();
+            $table->boolean('status')->default(true);
+            $table->boolean('trash')->default(false);
             $table->timestamps();
         });
     }

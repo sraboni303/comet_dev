ee03206c766fa98d2ee8a3a5a7a6bb7e14b30751
sraboni303 <sraboni391@gmail.com>
2021-04-02 16:48:13 +0600

    store posts with featured

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,19 +31,56 @@ class PostController extends Controller
     // Store
     public function store(Request $request){
 
+        // dd($request->gallery);
+        // dd($request->image);
+
+
         // Validation Post
         $this->validate($request, [
             'title' => 'required|unique:posts|max:255',
             'content' => 'required',
         ]);
 
+
+        // Image & Gallery Manage
+        $image_name = '';
+        $gallery_names = [];
+
+        if($request->hasFile('image')){
+
+            $image = $request->file('image');
+            $image_name = md5(time().rand()) . '.' . $image->getClientOriginalExtension();
+            $image->move(public_path('media/posts/', $image_name));
+
+        }elseif($request->hasFile('gallery')){
+
+            foreach($request->file('gallery') as $gallery){
+               $gallery_name = md5(time().rand()) . '.' . $gallery->getClientOriginalExtension();
+               $gallery->move(public_path('media/posts/', $gallery_name));
+               array_push($gallery_names, $gallery_name);
+            }
+        }
+
+
+        // Featured Ready
+        $featured = [
+            'type' => $request->radio,
+            'image' => $image_name,
+            'gallery' => $gallery_names,
+            'video' => $request->video,
+            'audio' => $request->audio,
+        ];
+
+
         // Create Post
         Post::create([
             'title' => $request->input('title'),
             'slug' => Str::slug($request->input('title')),
             'content' => $request->input('content'),
+            'featured' => json_encode($featured),
         ]);
 
         return redirect()->back()->with('message', 'Post Created Successfully..');
+
     }
 }
